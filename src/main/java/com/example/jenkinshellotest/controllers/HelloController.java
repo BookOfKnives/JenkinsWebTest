626ca4596460e677fdc4da632d5f1eb6488ddcfe
Hans Victor Munck-Westh <durandal2nd@gmail.com>
2021-10-13 10:45:27 +0200

    kreativt push

--- src/main/java/com/example/jenkinshellotest/controllers/HelloController.java
+++ src/main/java/com/example/jenkinshellotest/controllers/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
 
     @GetMapping(value={"/", "/first", "/index"}) @ResponseBody
     public String first(){
-        return "hello world22!";
+        return "hello world22 lille mleemmrum!";
     }
 
 }
